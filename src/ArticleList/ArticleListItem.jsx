4feb7bfe09a8c6b5d43497a48b4c33e1b3ee12db
Haiwei Liu <hathaway@Haiweis-MacBook-Pro.local>
2020-02-22 20:58:37 -0800

    week-7

--- src/ArticleList/ArticleListItem.jsx
+++ src/ArticleList/ArticleListItem.jsx
@@ -3,23 +3,24 @@ import styles from "./ArticleListItem.module.css";
 import SlugButton from "./SlugButton";
 import ArticleImage from "./ArticleImage";
 
+
 const ArticleListItem = props => {
   return (
-    <html>
+    <section className={styles.flexContainer}>
       <div>
-        <body>
-          <ArticleImage url={props.article.image._url} title={props.article.title} />
-          <section>
-            <h1>{props.article.title}</h1>
+        <ArticleImage url={props.article.image._url} title={props.article.title} />
+      </div>
+
+      <div>
+        <h1>{props.article.title}</h1>
             <p>{props.article.shortText}</p>
             <time dateTime={props.article.pubYear}>{props.article.pubDate}</time>
-            <div id="button">
-              {<SlugButton slug={props.article.slug} buttonText={props.article.author} />}
-            </div>
-          </section>
-        </body>
       </div>
-    </html>
+      
+      <div id="button">
+        {<SlugButton slug={props.article.slug} buttonText={props.article.author} />}
+      </div>
+    </section>
   );
 };
 
